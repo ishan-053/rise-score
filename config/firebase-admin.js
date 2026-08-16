8a6ec0287ad3bd4cbffbebe9a97b378a8909d690
ishan-053 <sindeishan042@gmail.com>
2026-08-16 17:59:51 +0530

    admin.js update

--- config/firebase-admin.js
+++ config/firebase-admin.js
@@ -1,24 +1,20 @@
-const admin = require("firebase-admin");
+const { initializeApp, getApps, cert } = require("firebase-admin/app");
+const { getAuth } = require("firebase-admin/auth");
 
 let serviceAccount;
 
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-
-    serviceAccount =
-        JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+    serviceAccount = JSON.parse(
+        process.env.FIREBASE_SERVICE_ACCOUNT
+    );
 } else {
-
-    serviceAccount =
-        require("../firebase-service-account.json");
+    serviceAccount = require("../firebase-service-account.json");
 }
 
-if (!admin.apps.length) {
-
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+if (getApps().length === 0) {
+    initializeApp({
+        credential: cert(serviceAccount)
     });
-
 }
 
-module.exports = admin.auth();
+module.exports = getAuth();
